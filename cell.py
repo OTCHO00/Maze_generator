992d7da29da71e66d3ea81642173c814a60c1a41
Fares OTCHO <faressbezzaouya@gmail.com>
2025-11-07 03:06:15 +0100

    Création de la class Cell

---
+++ cell.py
@@ -0,0 +1,38 @@
+import pygame
+from config import NB_LIGNES, NB_COLONNES, C_MUR
+
+class Cell:
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+        self.wall = { "N" : True,
+                      "S" : True,
+                      "E" : True,
+                      "W" : True
+                    }
+        self.visited = False
+        self.distance = float("inf")
+        self.parent = None
+        self.f_score = 0
+        self.g_score = 0
+
+    def remove_wall(self, direction):
+
+        self.wall[direction] = False
+
+    def has_wall(self, direction):
+
+        return self.wall[direction]
+
+    def draw(self, fenetre, cell_size):
+
+        x, y = self.col * cell_size, self.row * cell_size
+
+        if self.has_wall("N"):
+            pygame.draw.line(fenetre, C_MUR, (x, y), (x + cell_size, y))
+        if self.has_wall("S"):
+            pygame.draw.line(fenetre, C_MUR, (x, y + cell_size), (x + cell_size, y + cell_size))
+        if self.has_wall("E"):
+            pygame.draw.line(fenetre, C_MUR, (x + cell_size, y), (x + cell_size, y + cell_size))
+        if self.has_wall("W"):
+            pygame.draw.line(fenetre, C_MUR, (x, y), (x, y + cell_size))
